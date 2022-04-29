a967432c16201b2ebe815cc470023fafd2795d6e
Trey Williams <kingproto3232@gmail.com>
2022-04-29 12:32:51 -0400

    final

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle(`open`);
 }
+// $(".nav-mobile-about").click(function() {
+//  $([document.documentElement,document.body]).animate({
+//      scrollTop: $("#section2").offset().top}, 2000);
+// });
